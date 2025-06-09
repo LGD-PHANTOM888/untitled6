037b773e837cc9a5f7d3c45df4b82c12e54448ae
Артур <kpro83665@gmail.com>
2025-06-09 16:08:13 +0300

    Update StringBuilder.java

--- src/StringBuilder.java
+++ src/StringBuilder.java
@@ -1,25 +1,17 @@
-public class UserBuilder {
+public class StringBuilder {
     private String name;
     private int age;
     private String status;
-
-    public UserBuilder(String name, int age, String status) {
+    public StringBuilder(String name, int age, String status) {
         this.name = name;
         this.age = age;
         this.status = status;
     }
-
     public String buildInfo() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[Ім’я: ").append(name)
-                .append(" | Вік: ").append(age)
-                .append(" | Статус: ").append(status)
-                .append("]");
-        return sb.toString();
+        return "[Ім’я: " + name + " | Вік: " + age + " | Статус: " + status + "]";
     }
-
     public static void main(String[] args) {
-        UserBuilder builder = new UserBuilder("Олег", 25, "Активний");
+        StringBuilder builder = new StringBuilder("Олег", 25, "Активний");
         System.out.println(builder.buildInfo());
     }
 }
